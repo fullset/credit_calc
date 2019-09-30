4981c62f23fe8ac00d5cc9ff297d6f09a7706b54
fullset <fullset@gmail.com>
2019-09-30 15:28:42 +0300

    Add better finish date calculation.

--- Stage2/credit_calc.py
+++ Stage2/credit_calc.py
@@ -1,4 +1,28 @@
 import datetime
+from calendar import monthrange
+
+
+def calc_date(
+        year_start: int,
+        month_start: int,
+        day_start: int,
+        periods_count: int
+) -> datetime:
+    temp_date = datetime.datetime(year_start + int(month_start / 12), int((month_start % 12) + 1), 1)
+    for i in range(periods_count - 1):
+        temp_date = datetime.datetime(
+            temp_date.year + int(temp_date.month / 12),
+            int((temp_date.month % 12) + 1),
+            1
+        )
+
+    days_current_month = monthrange(temp_date.year, temp_date.month)[1]
+
+    if days_current_month < day_start:
+        return datetime.datetime(temp_date.year, temp_date.month, days_current_month)
+    else:
+        return datetime.datetime(temp_date.year, temp_date.month, day_start)
+
 
 principal = int(input('Enter principal of credit:'))
 periods = int(input('Enter count of periods:'))
@@ -10,7 +34,7 @@ print(year)
 print(month)
 print(day)
 
-repayment_date = datetime.datetime(year + int(periods / 12), int((periods % 12) + month), day)
+repayment_date = calc_date(year, month, day, periods)
 
 i = interest / 1200
 K = (i * ((1 + i) ** periods)) / ((1 + i) ** periods - 1)
